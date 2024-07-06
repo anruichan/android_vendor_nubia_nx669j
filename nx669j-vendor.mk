57db73a1ad5acdd0ead65674c099cc3cb5ba26a8
Bruno Martins <bgcngm@gmail.com>
2024-07-06 15:27:25 +0100

    nx669j: Remove software OMX blobs
    Default to C2 codecs from swcodec apex.
    
    Change-Id: I32f0b0abe680c1f350268115a3cc5488a2a57ec3

--- nx669j-vendor.mk
+++ nx669j-vendor.mk
@@ -513,22 +513,14 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0-impl \
     vendor.qti.hardware.fm@1.0-impl \
     vulkan.adreno \
-    libAlacSwDec \
-    libApeSwDec \
     libC2D2 \
     libCB \
-    libFlacSwDec \
-    libMpeghSwEnc \
-    libOmxAacDec \
     libOmxAlacDec \
-    libOmxAlacDecSw \
     libOmxAmrDec \
     libOmxAmrwbplusDec \
     libOmxApeDec \
-    libOmxApeDecSw \
     libOmxEvrcDec \
     libOmxG711Dec \
-    libOmxMpeghEncSw \
     libOmxQcelp13Dec \
     libOmxWmaDec \
     libOpenCL \
@@ -628,7 +620,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspmhal@1.0 \
     vendor.qti.voiceprint@1.0 \
     libOmxDsdDec \
-    libOmxMpeghDecSw \
     libadm \
     libadpcmdec \
     libdrc \
