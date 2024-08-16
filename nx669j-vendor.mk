4d20d7c1bc2fbe0b305249546c3f8473513198f1
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:03:24 +0300

    nx669j: fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09

--- nx669j-vendor.mk
+++ nx669j-vendor.mk
@@ -1147,7 +1147,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsconfig@2.0 \
     vendor.qti.ims.rcsconfig@2.1 \
     vendor.qti.imsrtpservice@3.0-service-Impl \
-    vendor.qti.imsrtpservice@3.0 \
+    vendor.qti.imsrtpservice@3.0-vendor \
     vendor.qti.latency@2.0 \
     vendor.qti.latency@2.1 \
     vendor.qti.qspmhal@1.0-impl \
